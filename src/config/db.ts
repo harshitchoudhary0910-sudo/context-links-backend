02ef348ff0333575a9785fe36e690b8c7de36c3d
Harshit Choudhary <harshitchoudhary0910@gmail.com>
2026-08-24 00:08:41 +0530

    changed .env loading for production

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-export function connectToDatabase() {
+export async function connectToDatabase() {
     try {
-        mongoose.connect(process.env.MONGODB_URI! as string);
+       await mongoose.connect(process.env.MONGODB_URI! as string);
         console.log("Connected to database");
     } catch (error) {
         console.error("Error connecting to database:", error);
--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,33 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from './middlewares/error_middleware';
 import cors from "cors";
-
 import authRoutes from './routes/auth_routes';
 import linkRoutes from './routes/link_routes';
 import { connectToDatabase } from './config/db';
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 
-
-const app=express();
-connectToDatabase();
-
-
+const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 
-
 app.use(cookieParser());
-
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/links', linkRoutes);
-app.use(errorMiddleware);           
+app.use(errorMiddleware);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+async function main() {
+    await connectToDatabase();
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+}
+
+main();
